325995cc1442a3932d5288e62605d77979bb7664
RaihanFirzie777 <alraihan77@gmail.com>
2024-06-17 10:17:09 +0700

    Initial Commit

---
+++ AppPromo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppPromo
+//
+//  Created by admin on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
